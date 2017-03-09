4272f5fa3aa5c5e3be6d917e72ad02d326bc9c6c
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-08 20:36:05 -0600

    new params varible number game route, method, and html page

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -23,6 +23,20 @@ class ParamsGamesController < ApplicationController
     end
     render "params_games_numbers.html.erb"
   end
+  def params_games_numbers_variable_method
+    @user_variable = params["input_variable"].to_i
+     random_number = 36
+    if @user_variable == nil
+      @message = "You haven't entered anything yet!"
+    elsif @user_variable == random_number
+      @message = "You won!"
+    elsif @user_variable < random_number
+      @message = "Higher guess, please."
+    elsif @user_variable > random_number
+      @message = "Lower guess, please."
+    end
+    render "params_games_numbers_variable.html.erb"
+  end
   def home_method
     @url_variable = params["this_is_a_variable"]
     render "homepage.html.erb"
