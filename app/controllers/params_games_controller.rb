35c2472fe45329daf6e3949ab9d495b00df15b11
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-08 19:00:07 -0600

    adding if statement for users that input a name starting with a

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,6 +1,7 @@
 class ParamsGamesController < ApplicationController
   def params_games_method
     @user_name = params["myname"]
+    @a_name = "Hey, your name starts with an A!"
     render "params_games.html.erb"
   end
 end
