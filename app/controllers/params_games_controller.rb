42b1c022b4f326665b10dd9ba943bf6885029275
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-03-08 18:52:16 -0600

    params name game working

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,2 +1,6 @@
 class ParamsGamesController < ApplicationController
+  def params_games_method
+    @user_name = params["myname"]
+    render "params_games.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/query_params_name" => "params_games#params_games_method"
 end
